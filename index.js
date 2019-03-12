a2bfdb38890cde39536a46ce30c53bdbbef80cab
Andrew Waller <andrewhwaller@gmail.com>
2019-03-12 15:35:56 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ function nestedTarget() {
 function increaseRankBy(n) {
   const list = document.querySelectorAll('ranked-list');
 
-  for (let i = 0; i +< list.length; i++) {
+  for (let i = 0; i < list.length; i++) {
     list[i].innerHTML = (parseInt([i]) + n);
   }
 }
