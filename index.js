bc0959e812d7e1dc1ba37833d5ddf0a9e24bcffa
Andrew Waller <andrewhwaller@gmail.com>
2019-03-12 15:18:25 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ function increaseRankBy(n) {
   const list = document.querySelectorAll('ranked-list');
 
   for (let i = 0; i < list.length; i++) {
-    list[i].innerHTML = (i + n);
+    list[i].innerHTML = (i + parseInt(n));
   }
 }
 
