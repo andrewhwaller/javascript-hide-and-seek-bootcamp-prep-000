b417f5626b92ecaa39eaaac030d7030d39f60c0f
Andrew Waller <andrewhwaller@gmail.com>
2019-03-12 15:17:11 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
+function getFirstSelector(selector) {
+  return document.querySelector(selector)
+}
 
+function nestedTarget() {
+  return document.querySelector('#nested .target')
+}
+
+function increaseRankBy(n) {
+  const list = document.querySelectorAll('ranked-list');
+
+  for (let i = 0; i < list.length; i++) {
+    list[i].innerHTML = (i + n);
+  }
+}
+
+function deepestChild() {
+
+}
