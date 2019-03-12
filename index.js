d1f4ba5d5c76add273a10462a9c2a35c53f46134
Andrew Waller <andrewhwaller@gmail.com>
2019-03-12 16:06:02 +0000

    Done.

--- index.js
+++ index.js
@@ -7,13 +7,25 @@ function nestedTarget() {
 }
 
 function increaseRankBy(n) {
-  const list = document.querySelectorAll('ranked-list');
+  const rankedLists = document.querySelectorAll('.ranked-list');
 
-  for (let i = 0; i < list.length; i++) {
-    list[i].innerHTML = (i + n);
+  for (let i = 0, l = rankedLists.length; i < l; i++) {
+    let children = rankedLists[i].children;
+
+    for (let j = 0, k = children.length; j < k; j++) {
+      children[j].innerHTML = parseInt(children[j].innerHTML) + n;
+    }
   }
 }
 
 function deepestChild() {
+  let node = document.getElementById('grand-node');
+  let nextNode = node.children[0];
 
+  while (nextNode) {
+    node = nextNode;
+    nextNode = node.children[0];
+  }
+
+  return node;
 }
